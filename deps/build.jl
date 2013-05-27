16134f89d6780c3ee9e881b7a2cf6a92bed0e3c0
Viral B. Shah <viral@mayin.org>
2013-05-27 09:48:58 +0530

    Attempt to detect and build only tk and tk_wrapper are not available.

--- deps/build.jl
+++ deps/build.jl
@@ -1,62 +1,75 @@
 using BinDeps
 
-s = @build_steps begin
-    c=Choices(Choice[Choice(:skip,"Skip Installation - Binaries must be installed manually",nothing)])
-end
-
-## Homebrew
-@osx_only push!(c,Choice(:brew,"Install depdendency using brew",@build_steps begin
-        HomebrewInstall("tk",ASCIIString["--enable-aqua"])
-        end))
-
-## Prebuilt Binaries
-depsdir = joinpath(Pkg.dir(),"Tk","deps")
-@windows_only begin
-    local_file = joinpath(joinpath(depsdir,"downloads"),"Tk.tar.gz")
-    push!(c,Choice(:binary,"Download prebuilt binary",@build_steps begin
-                ChangeDirectory(depsdir)
-                FileDownloader("http://julialang.googlecode.com/files/Tk.tar.gz",local_file)
-                FileUnpacker(local_file,joinpath(depsdir,"usr"))
-            end))
-end
-
-prefix=joinpath(depsdir,"usr")
-uprefix = replace(replace(prefix,"\\","/"),"C:/","/c/")
-
-## Install from source
-let 
-    steps = @build_steps begin ChangeDirectory(depsdir) end
-
-    ENV["PKG_CONFIG_LIBDIR"]=ENV["PKG_CONFIG_PATH"]=joinpath(depsdir,"usr","lib","pkgconfig")
-    @unix_only ENV["PATH"]=joinpath(prefix,"bin")*":"*ENV["PATH"]
-    @windows_only ENV["PATH"]=joinpath(prefix,"bin")*";"*ENV["PATH"]
-    ## Windows Specific dependencies
-
-    is64bit = WORD_SIZE == 64
-    tcldir = OS_NAME==:Windows?"tcl8.6.0/win":OS_NAME==:Darwin?"tcl8.6.0/macosx":"tcl8.6.0/unix"
-    tkdir = OS_NAME==:Windows?"tk8.6.0/win":OS_NAME==:Darwin?"tk8.6.0/macosx":"tk8.6.0/unix"
-
-    steps |= @build_steps begin
-        ChangeDirectory(depsdir)
-        autotools_install(depsdir,"http://prdownloads.sourceforge.net/tcl/tcl8.6.0-src.tar.gz","tcl-8.6.0.tar.gz",String[is64bit ? "--enable-64bit" : "--disable-64bit","--enable-threads","--enable-symbols","TCL_BIN_DIR=\"$uprefix/bin\""],"tcl8.6.0",tcldir,"libtcl8.6."*BinDeps.shlib_ext,"libtcl8.6."*BinDeps.shlib_ext)
-        begin
-            ChangeDirectory(joinpath("builds",tcldir))
-            `make install-private-headers`
-        end
-        autotools_install(depsdir,"http://prdownloads.sourceforge.net/tcl/tk8.6.0-src.tar.gz","tk-8.6.0.tar.gz",String["--enable-symbols",is64bit ? "--enable-64bit" : "--disable-64bit","--enable-threads","--enable-aqua=yes","--with-tcl="*joinpath(uprefix,"lib")],"tk8.6.0",tkdir,"libtk8.6."*BinDeps.shlib_ext,"libtk8.6."*BinDeps.shlib_ext)
-        begin
-            ChangeDirectory(joinpath("builds",tkdir))
-            `make install-private-headers`
+function find_library(libname,filename)
+    try 
+        dl = dlopen(joinpath(Pkg.dir(),"Tk","deps","usr","lib",filename))
+    catch
+        try 
+            dl = dlopen(libname)
+            dlclose(dl)
+        catch
+            return false
         end
     end
-
-    push!(c,Choice(:source,"Install depdendency from source",steps))
+    return true
 end
 
-try
-    dl = dlopen("libtk_wrapper")
-    dlclose(dl)
-catch
+function build()
+    s = @build_steps begin
+        c=Choices(Choice[Choice(:skip,"Skip Installation - Binaries must be installed manually",nothing)])
+    end
+
+    ## Homebrew
+    @osx_only push!(c,Choice(:brew,"Install depdendency using brew",@build_steps begin
+        HomebrewInstall("tk",ASCIIString["--enable-aqua"])
+    end))
+
+    ## Prebuilt Binaries
+    depsdir = joinpath(Pkg.dir(),"Tk","deps")
+    @windows_only begin
+        local_file = joinpath(joinpath(depsdir,"downloads"),"Tk.tar.gz")
+                   push!(c,Choice(:binary,"Download prebuilt binary",@build_steps begin
+                       ChangeDirectory(depsdir)
+                       FileDownloader("http://julialang.googlecode.com/files/Tk.tar.gz",local_file)
+                       FileUnpacker(local_file,joinpath(depsdir,"usr"))
+         end))
+    end
+
+    prefix=joinpath(depsdir,"usr")
+    uprefix = replace(replace(prefix,"\\","/"),"C:/","/c/")
+    
+    ## Install from source
+    let 
+        steps = @build_steps begin ChangeDirectory(depsdir) end
+
+        ENV["PKG_CONFIG_LIBDIR"]=ENV["PKG_CONFIG_PATH"]=joinpath(depsdir,"usr","lib","pkgconfig")
+        @unix_only ENV["PATH"]=joinpath(prefix,"bin")*":"*ENV["PATH"]
+        @windows_only ENV["PATH"]=joinpath(prefix,"bin")*";"*ENV["PATH"]
+        ## Windows Specific dependencies
+
+        is64bit = WORD_SIZE == 64
+        tcldir = OS_NAME==:Windows?"tcl8.6.0/win":OS_NAME==:Darwin?"tcl8.6.0/macosx":"tcl8.6.0/unix"
+        tkdir = OS_NAME==:Windows?"tk8.6.0/win":OS_NAME==:Darwin?"tk8.6.0/macosx":"tk8.6.0/unix"
+
+        steps |= @build_steps begin
+            ChangeDirectory(depsdir)
+            autotools_install(depsdir,"http://prdownloads.sourceforge.net/tcl/tcl8.6.0-src.tar.gz","tcl-8.6.0.tar.gz",String[is64bit ? "--enable-64bit" : "--disable-64bit","--enable-threads","--enable-symbols","TCL_BIN_DIR=\"$uprefix/bin\""],"tcl8.6.0",tcldir,"libtcl8.6."*BinDeps.shlib_ext,"libtcl8.6."*BinDeps.shlib_ext)
+            begin
+                ChangeDirectory(joinpath("builds",tcldir))
+                `make install-private-headers`
+            end
+            autotools_install(depsdir,"http://prdownloads.sourceforge.net/tcl/tk8.6.0-src.tar.gz","tk-8.6.0.tar.gz",String["--enable-symbols",is64bit ? "--enable-64bit" : "--disable-64bit","--enable-threads","--enable-aqua=yes","--with-tcl="*joinpath(uprefix,"lib")],"tk8.6.0",tkdir,"libtk8.6."*BinDeps.shlib_ext,"libtk8.6."*BinDeps.shlib_ext)
+            begin
+                ChangeDirectory(joinpath("builds",tkdir))
+                `make install-private-headers`
+            end
+        end
+
+        push!(c,Choice(:source,"Install depdendency from source",steps))
+    end
+end
+
+function build_wrapper()
     cc = CCompile("src/tk_wrapper.c","$prefix/lib/libtk_wrapper."*BinDeps.shlib_ext,
                   ["-shared","-g","-fPIC","-I$prefix/include",
                    "-I/usr/local/include",
@@ -82,5 +95,14 @@ catch
     end
 
     run(s)
-
 end
+
+
+builddeps = false
+
+if !find_library(OS_NAME == :Linux ? "libtcl8.5" : "libtcl", OS_NAME == :Windows ? "tcl86g" : "libtcl8.6"); builddeps = true; end
+if !find_library(OS_NAME == :Linux ? "libtk8.5" : "libtk", OS_NAME == :Windows ? "tk86g" : "libtk8.6"); builddeps = true; end
+
+if builddeps; build(); end
+
+if !find_library("libtk_wrapper","libtk_wrapper"); build_wrapper(); end
