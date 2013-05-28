e8e5da01f22cb41dab8dd7daab22cdaf6cb30f61
Viral B. Shah <viral@mayin.org>
2013-05-28 13:57:04 +0530

    Update filenames on OS X.

--- deps/ext.jl
+++ deps/ext.jl
@@ -12,9 +12,9 @@ let
             end
         end
     end
-    find_library(OS_NAME == :Linux ? "libtcl8.5" : "libtcl",
+    find_library(OS_NAME == :Linux ? "libtcl8.5" : OS_NAME == :Darwin ? "libtcl8.6" : "libtcl",
                  OS_NAME == :Windows ? "tcl86g" : "libtcl8.6")
-    find_library(OS_NAME == :Linux ? "libtk8.5" : "libtk",
+    find_library(OS_NAME == :Linux ? "libtk8.5" : OS_NAME == :Darwin ? "libk8.6" : "libtk",
                  OS_NAME == :Windows ? "tk86g" : "libtk8.6")
     find_library("libtk_wrapper","libtk_wrapper")
 end
