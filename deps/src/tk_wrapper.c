7ce3916db8b714bae50b159fad4f41d32eea1110
Jameson Nash <vtjnash@gmail.com>
2013-07-14 03:40:46 -0400

    simplify tk_wrapper so we can build against MacPorts and system Tk, without special headers

--- deps/src/tk_wrapper.c
+++ deps/src/tk_wrapper.c
@@ -13,7 +13,7 @@ void *jl_tkwin_visual(Tk_Window tkwin)
     return Tk_Visual(tkwin);
 }
 
-int jl_tkwin_id(Tk_Window tkwin)
+int jl_tkwin_id(Tk_Window tkwin) // XXX: THIS IS NOT AN int ON MAC
 {
     return Tk_WindowId(tkwin);
 }
@@ -21,26 +21,13 @@ int jl_tkwin_id(Tk_Window tkwin)
 #ifdef __WIN32__
 
 HWND globalHWND;
-
 #include "TkWinInt.h"
 
 void *jl_tkwin_hdc(Tk_Window tkwin, Display *display)
 {
-    HDC dc;
-    TkWinDCState state;
-    PAINTSTRUCT PS;
-    RECT rect, rect2;
     HWND win = ((TkWinDrawable *)Tk_WindowId(tkwin))->window.handle;
-    GetClientRect(win,&rect);
-    InvalidateRect(win,&rect,FALSE);
-    HRGN rgn = CreateRectRgn(rect.left,rect.top,rect.right,rect.bottom);
-    dc = GetDC(win);
-    int ok = SelectClipRgn(dc,rgn);
-    GetClipBox(dc,&rect2);
-    SetBkMode(dc,1);
-    TextOut(dc,10,10,"Hello World",11);
+    HDC dc = GetDC(win);
     return dc;
-    //return TkWinGetHDC(Tk_WindowId(tkwin));
 }
 
 void *jl_tkwin_hdc_release(HDC hdc) 
@@ -52,40 +39,19 @@ void *jl_tkwin_hdc_release(HDC hdc)
 
 #ifdef __APPLE__
 
-#define MAC_OSX_TK
 #import <Cocoa/Cocoa.h>
 #include "ApplicationServices/ApplicationServices.h"
-#include "tkMacOSXInt.h"
 
-NSView *drawableView(MacDrawable *macWin)
-{
-    NSView *view;
-    if (!macWin) {
-        view = NULL;
-    }
-    else if (!macWin->toplevel) {
-        view = macWin->view;
-    }
-    else if (!(macWin->toplevel->flags & TK_EMBEDDED)) {
-        view = macWin->toplevel->view;
-    }
-    else {
-        TkWindow *contWinPtr = TkpGetOtherWindow(macWin->toplevel->winPtr);
-        if (contWinPtr) {
-            view = drawableView(contWinPtr->privatePtr);
-        }
-    }
-    return view;
-}
+typedef struct TkRegion_ *TkRegion;
+#define MAC_OSX_TK
+#include "tkMacOSX.h"
 
-CGContextRef getView(TkWindow *winPtr, int height)
+CGContextRef getView(Tk_Window *winPtr, int height)
 {
-    CGContextRef context;
-    NSView *view;
     int focusLocked;
     int dontDraw = 0;
-    MacDrawable *macWin = (MacDrawable *) winPtr->window;
-    view = drawableView(macWin);
+
+    NSView *view = TkMacOSXGetRootControl(Tk_WindowId(winPtr));
 
     if (view) {
         if (view != [NSView focusView]) {
@@ -99,7 +65,7 @@ CGContextRef getView(TkWindow *winPtr, int height)
             return NULL;
         }
     }
-    context = [[[view window] graphicsContext] graphicsPort];
+    CGContextRef context = [[[view window] graphicsContext] graphicsPort];
     return context;
 }
 
