d1e51cf1e6162fa1960fcd2c2d844ec420a9e0cf
Benjamin Deonovic <bdeonovic@gmail.com>
2014-10-20 20:31:46 -0500

    Use tk8.5 rather than tk8.6

--- deps/build.jl
+++ deps/build.jl
@@ -2,8 +2,8 @@ using BinDeps
 
 @BinDeps.setup
 
-tcl = library_dependency("tcl",aliases=["libtcl8.6","tcl86g","tcl86t","libtcl","libtcl8.6.so.0","libtcl8.5","libtcl8.5.so.0","tcl85"])
-tk = library_dependency("tk",aliases=["libtk8.6","libtk","libtk8.6.so.0","libtk8.5","libtk8.5.so.0","tk85","tk86","tk86t"], depends=[tcl], validate = function(p,h)
+tcl = library_dependency("tcl",aliases=["libtcl","libtcl8.5","libtcl8.5.so.0","tcl85"])
+tk = library_dependency("tk",aliases=["libtk","libtk8.5","libtk8.5.so.0","tk85"], depends=[tcl], validate = function(p,h)
     @osx_only return dlsym_e(h,:TkMacOSXGetRootControl) != C_NULL
     return true
 end)
@@ -14,8 +14,8 @@ end)
     provides(WinRPM.RPM,"tcl",tcl,os = :Windows)
 end
 
-provides(AptGet,"tcl8.6",tcl)
-provides(AptGet,"tk8.6",tk)
+provides(AptGet,"tcl8.5",tcl)
+provides(AptGet,"tk8.5",tk)
 
 provides(Sources,URI("http://prdownloads.sourceforge.net/tcl/tcl8.6.0-src.tar.gz"),tcl,unpacked_dir = "tcl8.6.0")
 provides(Sources,URI("http://prdownloads.sourceforge.net/tcl/tk8.6.0-src.tar.gz"),tk,unpacked_dir = "tk8.6.0")
@@ -32,7 +32,7 @@ if WORD_SIZE == 64
         provides(BuildProcess,(@build_steps begin
                 GetSources(tcl)
                 FileRule("tclConfig.sh",@build_steps begin
-                        ChangeDirectory(joinpath(srcdir(tcl),"tcl8.6.0/win"))
+                        ChangeDirectory(joinpath(srcdir(tcl),"tcl8.5.0/win"))
                         `nmake -f Makefile.vc`
                         `nmake -f Makefile.vc install INSTALLDIR=$(usrdir(tcl))`
                         `xcopy $(libdir(tcl))\\tclConfig.sh $(srcdir(tcl))\\tcl8.6.0\\win`
@@ -41,14 +41,14 @@ if WORD_SIZE == 64
         provides(BuildProcess,(@build_steps begin
                 GetSources(tk)
                 begin
-                        ChangeDirectory(joinpath(srcdir(tk),"tk8.6.0/win"))
-                        `nmake -f Makefile.vc TCLDIR=$(srcdir(tcl))\\tcl8.6.0`
+                        ChangeDirectory(joinpath(srcdir(tk),"tk8.5.0/win"))
+                        `nmake -f Makefile.vc TCLDIR=$(srcdir(tcl))\\tcl8.5.0`
                         `nmake -f Makefile.vc install INSTALLDIR=$(usrdir(tk))`
                 end
         end),tk)
 else
-        provides(BuildProcess,Autotools(libtarget = "tcl86.dll", configure_subdir = "win", configure_options = [is64bit?"--enable-64bit":"--disable-64bit","--enable-threads"]),tcl, os = :Windows)
-        provides(BuildProcess,Autotools(libtarget = "tk86.dll", configure_subdir = "win", configure_options = [is64bit?"--enable-64bit":"--disable-64bit"]),tk, os = :Windows)
+        provides(BuildProcess,Autotools(libtarget = "tcl85.dll", configure_subdir = "win", configure_options = [is64bit?"--enable-64bit":"--disable-64bit","--enable-threads"]),tcl, os = :Windows)
+        provides(BuildProcess,Autotools(libtarget = "tk85.dll", configure_subdir = "win", configure_options = [is64bit?"--enable-64bit":"--disable-64bit"]),tk, os = :Windows)
 end
 
 @BinDeps.install Dict([:tk => :libtk, :tcl=>:libtcl])
