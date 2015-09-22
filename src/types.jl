5f780e9af59d2cbfd76d797480c7f0344cbfb706
Rene Donner <code@donner.at>
2015-09-22 10:13:24 +0200

    fix for 0.4rc2

--- src/types.jl
+++ src/types.jl
@@ -3,13 +3,13 @@ abstract Tk_Widget
 abstract TTk_Widget <: Tk_Widget ## for ttk::widgets
 abstract TTk_Container <: Tk_Widget ## for containers (frame, labelframe, ???)
 
-Widget = Union(TkWidget, Tk_Widget, Canvas, String)
+Widget = (@compat Union{TkWidget, Tk_Widget, Canvas, AbstractString})
 
 ## Maybe -- can this be parameterized?
 ## https://groups.google.com/forum/?fromgroups=#!topic/julia-dev/IbbWwplrqlc (takeaway -- this style is frowned upon)
-MaybeFunction = Union(Function, Nothing)
-MaybeString = Union(String, Nothing)
-MaybeStringInteger = Union(String, Integer, Nothing) # for at in tree insert
-MaybeVector = Union(Vector, Nothing)
-MaybeWidget = Union(Widget, Nothing)
-MaybeBool = Union(Bool, Nothing)
+MaybeFunction = (@compat Union{Function, (@compat Void)})
+MaybeString = (@compat Union{AbstractString, (@compat Void)})
+MaybeStringInteger = (@compat Union{AbstractString, Integer, (@compat Void)}) # for at in tree insert
+MaybeVector = (@compat Union{Vector, (@compat Void)})
+MaybeWidget = (@compat Union{Widget, (@compat Void)})
+MaybeBool = (@compat Union{Bool, (@compat Void)})
